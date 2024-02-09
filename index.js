d32aa1519d41e4cccbbfda7bf057207704b85416
Steph01 <roy.guevara2903@gmail.com>
2024-02-09 17:08:33 -0600

    Modificando el dist

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'dist')))
+
 app.use(morgan((tokens, req, res) => {
     return [
         tokens.method(req, res),
